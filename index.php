e518f4dd811f5f2f0a227f6a3328e39b61ab2a75
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-09 16:38:22 +0200

    milestone 1 completa

--- index.php
+++ index.php
@@ -8,13 +8,40 @@ include __DIR__ . '/API/dischi.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./asset/style.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-    foreach($database as $dischi) {
-        echo "<div>" . $dischi['title'] . "</div>";
-    }
-    ?>
+    <header>
+        <div>
+            <img src="./asset/img/logo-small.svg" alt="Spotify Logo" class="logo">
+        </div>
+    </header>
+    <main class="overflow-auto">
+        <div class="container">
+            <div class="row row-cols-5 py-5 gx-5">
+            <?php
+                foreach($database as $dischi) {
+                    echo "<div class='px-4 my-3'>
+                            <div class='card h-100'>
+                                <img src=" . $dischi['poster'] ."  class='img-fluid p-3'alt='...'>
+                                <div class='card-body text-center '>
+                                    <h5 class='card-title'>" . $dischi['title'] . "</h5>
+                                    <p class='card-text'>" . $dischi['author'] . "</p>
+                                    <p class='card-text'>" . $dischi['year'] . "</p>
+                                </div>
+                            </div>
+                        </div>";
+                };
+            ?>
+            </div>
+        </div>
+    </main>
+    
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </body>
 </html>
